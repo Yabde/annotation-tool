60ea3337a318cf63074753cb561acf4c38e9fceb
Yabde <yassine04.97@hotmail.fr>
2021-03-17 15:34:19 +0100

    test

--- api/src/server.ts
+++ api/src/server.ts
@@ -34,7 +34,7 @@ function loadFront(): void {
     });
 }
 
-const {PORT = 3000} = process.env;
+const {PORT = 3001} = process.env;
 const server = http.createServer(router);
 
 server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`));
